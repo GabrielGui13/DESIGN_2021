3a4c9d3a57789991ca5b7dedf3144cd636958278
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-03-22 10:03:22 -0300

    atividade aula 18

---
+++ Atividade Aula 18/q1.js
@@ -0,0 +1,7 @@
+function questao1() {
+    var pergunta = prompt("Você está gostando do curso?")
+    if (pergunta == "sim") {
+        alert("Ficamos imensamente felizes que esteja gostando!")
+    }
+else alert("Poxa, em que podemos melhorar?")
+}
